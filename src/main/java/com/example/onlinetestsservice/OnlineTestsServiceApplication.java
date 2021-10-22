e43dde5e3ff1bfa069f4178b368216683363c992
AlexeySkridlevskiy <skrid71@gmail.com>
2021-10-22 12:18:33 +0300

    commit app files

---
+++ src/main/java/com/example/onlinetestsservice/OnlineTestsServiceApplication.java
@@ -0,0 +1,22 @@
+package com.example.onlinetestsservice;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class OnlineTestsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineTestsServiceApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            System.out.println("Hello world!");
+        };
+    }
+}
---
+++ src/test/java/com/example/onlinetestsservice/OnlineTestsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinetestsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineTestsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
