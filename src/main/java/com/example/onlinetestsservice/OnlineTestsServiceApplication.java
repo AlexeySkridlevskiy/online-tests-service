d9faf9d85efc9a9cfdd12742f9203ca7ff31a7bb
AlexeySkridlevskiy <skrid71@gmail.com>
2021-10-22 15:12:40 +0300

    commit HelloController

--- src/main/java/com/example/onlinetestsservice/OnlineTestsServiceApplication.java
+++ src/main/java/com/example/onlinetestsservice/OnlineTestsServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.onlinetestsservice;
 
+import java.util.Arrays;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,7 +18,16 @@ public class OnlineTestsServiceApplication {
     @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Hello world!");
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
         };
     }
+
 }
